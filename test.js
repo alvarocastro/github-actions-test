b3d96e5f2023d84b5743caf1ce413566621a3f6d
Alvaro Castro <moltar89@gmail.com>
2024-01-17 19:35:53 -0300

    feat: feature 20, fail

--- test.js
+++ test.js
@@ -13,6 +13,8 @@ test('Resta should subtract two numbers', t => {
 	t.is(resta(4, 1), 3);
 	t.is(resta(-1, 2), -3);
 	t.is(resta(3, -3), 6);
+
+	t.true(false);
 	// Removed case t.is(resta(0.1, 0.2), -0.1);
 });
 
